4697e3324306022fe5f244421cf5482363552b5e
Minh Hoang <hcminh3003@gmail.com>
2024-06-19 14:07:50 +0700

    update preview file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,72 @@
 'use client'
+import React, { useState } from 'react';
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
-import './styles.css'; // Đường dẫn tới tệp CSS của bạn
+import './styles.css'; // Path to your CSS file
+
+interface Document {
+  uri: string;
+  fileType: 'pptx' | 'xlsx' | 'pdf' | 'docx' | 'png' | 'txt';
+  name?: string;
+}
 
 function App() {
-  const docs = [
+  const [currentDoc, setCurrentDoc] = useState<Document | null>(null);
+
+  const docs: Document[] = [
     { 
-      uri: "https://github.com/cyntler/react-doc-viewer/files/9015134/test-doc.docx",
-      fileType: 'docx'
-    }, // Remote file
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/samplepptx_1718719981.pptx",
+      fileType: 'pptx',
+      name: 'samplepptx_1718719981.pptx'
+    },
     { 
-      uri: "https://github.com/cyntler/react-doc-viewer/files/9015135/test-excelaki.xlsx",
-      fileType: 'xlsx'
-    }, // Remote file
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/Individuals.xlsx",
+      fileType: 'xlsx',
+      name: 'Individuals.xlsx'
+    },
     { 
-      uri: "http://localhost:3000/documents/small.pdf",
-      fileType: 'pdf'
-    }, // Local file
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/small.pdf",
+      fileType: 'pdf',
+      name: 'small.pdf'
+    },
+    // Uncomment additional documents as needed
+    { 
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/sample.docx",
+      fileType: 'docx',
+      name: 'sample.docx'
+    },
+    { 
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/Screenshot%20from%202024-06-14%2017-14-05.png",
+      fileType: 'png',
+      name: 'Screenshot from 2024-06-14 17-14-05.png'
+    },
+    { 
+      uri: "https://gmt-s3.s3.ap-southeast-1.amazonaws.com/master-contract/Unit%20Test%20l%C3%A0%20m%E1%BB%99t%20lo%E1%BA%A1i.txt",
+      fileType: 'txt',
+      name: 'TxtFile'
+    }
   ];
 
-  console.log(docs[0].uri);
-  // https://github.com/cyntler/react-doc-viewer/files/9015135/test-excelaki.xlsx
-  // https://rotciv.me/TH_Day1.docx
+  const handleDocClick = (doc: Document) => {
+    setCurrentDoc(doc);
+  };
 
   return (
     <div className="docViewerContainer">
-      <DocViewer
-        documents={docs}
-        pluginRenderers={DocViewerRenderers}
-        prefetchMethod="GET"
-      />
+      <div className="buttonContainer">
+        {docs.map((doc, index) => (
+          <button key={index} onClick={() => handleDocClick(doc)}>
+            Preview - {doc.name || `Document ${index + 1}`}
+          </button>
+        ))}
+      </div>
+      {currentDoc && (
+        <DocViewer
+          documents={[currentDoc]}
+          pluginRenderers={DocViewerRenderers}
+        />
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
